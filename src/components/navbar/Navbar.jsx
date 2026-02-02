3839749c0791b2996ab667f79a4a435e819b204b
MoSu1248 <mohammedsuhailr2@gmail.com>
2026-02-02 19:14:41 +0200

    fixing files

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+import Nav from "../Nav/Nav";
+import NavToggle from "../navToggle/NavToggleHide";
+import ThemeToggler from "../themeToggle/ThemeToggler";
+import { useNavTogglerStore } from "../stores/useNavTogglerStore";
+import * as motion from "motion/react-client";
+
+export default function Navbar() {
+  const [showNav, setShowNav] = useState(true);
+  const navDisplay = useNavTogglerStore((state) => state.navDisplay);
+
+  return (
+    <motion.div className={navDisplay ? `navbar` : `navbar navbar__hidden`}>
+      <h3 className="navbar__heading">ALL BOARDS (3)</h3>
+      <Nav />
+      <ThemeToggler />
+      <NavToggle showNav={showNav} />
+    </motion.div>
+  );
+}

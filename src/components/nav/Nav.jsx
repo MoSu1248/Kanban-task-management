67d9415dd7f1ee4d57ac3f486cb6cad45802eae9
MoSu1248 <mohammedsuhailr2@gmail.com>
2026-02-02 19:30:14 +0200

    fixing files

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import "./Nav.scss";
+import Board from "../../assets/icon-board.svg?react";
+import { userBoardStore } from "../stores/useBoardStore";
+import { useNavTogglerStore } from "../stores/useNavTogglerStore";
+import { useModalStore } from "../stores/useModalStore";
+
+export default function Nav() {
+  const navDisplay = useNavTogglerStore((state) => state.navDisplay);
+  const modalOpen = useModalStore((state) => state.toggleModalOpen);
+
+  const { boards, fetchBoards, loading, selectedBoardId, setSelectedBoardId } =
+    userBoardStore();
+
+  useEffect(() => {
+    fetchBoards();
+  }, [fetchBoards]);
+
+  // if (loading) return <p>Loading...</p>;
+  // if (!boards.length) return <p>No boards found</p>;
+
+  return (
+    <ul className={navDisplay ? `list` : `list list-hide`}>
+      {boards.map((item, index) => (
+        <li
+          className={
+            selectedBoardId === item.name ? `list__item active` : `list__item`
+          }
+          key={index}
+          onClick={() => {
+            console.log(selectedBoardId);
+            setSelectedBoardId(item.name);
+          }}
+        >
+          <Board />
+          {item.name}
+        </li>
+      ))}
+      <li
+        className="list__item list__create"
+        onClick={() => modalOpen("ADD__BOARD")}
+      >
+        <Board />+ Create New Board
+      </li>
+    </ul>
+  );
+}

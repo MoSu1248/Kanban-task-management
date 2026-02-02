8b24e8be6a80cf08b825700bbb1265b5d49a04ef
MoSu1248 <mohammedsuhailr2@gmail.com>
2026-02-02 19:19:23 +0200

    fixing files

---
+++ src/components/navToggle/NavToggleHide.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./NavToggle.scss";
+import ToggleIcon_hide from "../../assets/icon-hide-sidebar.svg?react";
+import { useNavTogglerStore } from "../stores/useNavTogglerStore";
+
+export default function NavToggle() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const toggleNav = useNavTogglerStore((state) => state.toggleNav);
+
+  return (
+    <div className="nav__toggle">
+      <div className="nav__toggle-container" onClick={toggleNav}>
+        <ToggleIcon_hide />
+        <h3 className="nav__toggle-text">Hide Sidebar</h3>
+      </div>
+    </div>
+  );
+}
